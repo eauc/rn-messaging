108bcc582ed0804457f16a0bab05f8ebfa69ef93
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2018-10-30 23:36:12 +0100

    MessageList onPressItem

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  BackHandler,
+} from 'react-native';
 
 import Status from './components/Status';
 import MessageList from './components/MessageList';
 import { createImageMessage, createLocationMessage, createTextMessage } from './utils/MessageUtils';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    this.subscription = BackHandler.addEventListener('hardwareBackPress', () => {
+      const { fullscreenImageId } = this.state;
+      if (fullscreenImageId) {
+        this.dismissFullscreenImage();
+        return true;
+      }
+      return false;
+    });
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
   state = {
     messages: [
       createImageMessage('https://unsplash.it/300/300'),
@@ -16,9 +39,59 @@ export default class App extends React.Component {
         longitude: -122.4324,
       }),
     ],
+    fullscreenImageId: null,
   };
 
-  handlePressMessage = () => {}
+  dismissFullscreenImage = () => {
+    this.setState({ fullscreenImageId: null });
+  };
+
+  handlePressMessage = ({type, id}) => {
+    switch (type) {
+    case 'text':
+      Alert.alert(
+        'Delete message?',
+        'Are you sure you want to permanently delete this message?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'Delete',
+            style: 'destructive',
+            onPress: () => {
+              const { messages } = this.state;
+              this.setState({ messages: messages.filter(message => message.id !== id) });
+            },
+          },
+        ],
+      );
+      break;
+    case 'image':
+      this.setState({ fullscreenImageId: id });
+      break;
+    default:
+      break;
+    }
+  }
+
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state;
+    if (!fullscreenImageId) return null;
+
+    const image = messages.find(message => message.id === fullscreenImageId);
+    if (!image) return null;
+
+    const { uri } = image;
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullscreenImage}>
+        <Image style={styles.fullscreenImage} source={{ uri }} />
+      </TouchableHighlight>
+    );
+  };
 
   renderMessageList() {
     const { messages } = this.state;
@@ -48,6 +121,7 @@ export default class App extends React.Component {
         {this.renderMessageList()}
         {this.renderToolbar()}
         {this.renderInputMethodEditor()}
+        {this.renderFullscreenImage()}
       </View>
     );
   }
@@ -70,5 +144,14 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: 'rgba(0,0,0,0.04)',
     backgroundColor: 'white',
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 2,
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain',
   },
 });
